b5203a69d86b943d0811b8c235be059a7b8ed075
Thiago Caiubi <thiago.caiubi@gmail.com>
2022-04-02 11:31:56 -0300

    Remove empty line

--- tools/setup.sh
+++ tools/setup.sh
@@ -11,7 +11,6 @@ if ! command -v brew &>/dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-
 if ! command -v ansible-playbook &>/dev/null; then
     echo "Install Ansible"
     brew install ansible
