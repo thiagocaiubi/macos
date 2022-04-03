1b5b7aa4e35eaf5105d23a1d5f7911342acab753
Thiago Caiubi <thiago.caiubi@gmail.com>
2022-04-03 11:15:44 -0300

    Fix first install due to missing git

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 [macOS](https://www.apple.com/macos/) setup automation.
 
+## Install prerequisites
+
+```console
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/thiagocaiubi/macos/HEAD/tools/install.sh)"
+```
+
 ## Running
 
 ```console
-$ make setup
+make setup
 ```
---
+++ tools/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+xcode-select -p &>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Install Xcode"
+    xcode-select --install
+fi
+
+if ! command -v brew &>/dev/null; then
+    echo "Install Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if ! command -v git &>/dev/null; then
+    echo "Install git"
+    brew install git
+fi
+
+echo "Clone the repo, then follow the instructions in the README.md file"
+echo "git clone git@github.com:thiagocaiubi/macos.git"
